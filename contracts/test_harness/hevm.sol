b885059a9300f8dd68e6c236d25b3fda7e8535ee
sam bacha <sam@manifoldfinance.com>
2022-03-15 15:29:14 -0700

    Create hevm.sol

---
+++ contracts/test_harness/hevm.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.6;
+
+interface Hevm {
+    function warp(uint256) external;
+    function roll(uint256) external;
+    function store(address,bytes32,bytes32) external;
+    function sign(uint,bytes32) external returns (uint8,bytes32,bytes32);
+    function addr(uint) external returns (address);
+    function ffi(string[] calldata) external returns (bytes memory);
+}
