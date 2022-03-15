12ab72d0fb0948bc1b407a0134c54ad13c474d95
sam bacha <sam@manifoldfinance.com>
2022-03-15 15:27:59 -0700

    feat: foundry

---
+++ contracts/test_harness/ERC20.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.6;
+
+interface ERC20 {
+    function balanceOf(address usr) external view returns (uint);
+    function totalSupply() external view returns (uint);
+
+    function allowance(address src, address dst) external view returns (uint);
+    function approve(address dst, uint amt) external returns (bool);
+
+    function transfer(address dst, uint amt) external returns (bool);
+    function transferFrom(address src, address dst, uint amt) external returns (bool);
+
+    event Transfer(address indexed src, address indexed dst, uint amt);
+    event Approval(address indexed src, address indexed dst, uint amt);
+}
